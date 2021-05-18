e2ca7d326ee10455fbdefe5badf32aa84c8b4af0
Jesse Callahan <jessetylercallahan@gmail.com>
2021-05-17 20:40:02 -0700

    add menulink

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -1,12 +1,13 @@
 namespace BestRestaurants.Models
 {
-  public class Restaurant 
+  public class Restaurant
   {
-    public int RestaurantId {get;set;}
-    public string Name {get;set;}
-    public string Description {get;set;}
-    public int PriceRange {get;set;}
-      public int CuisineId { get; set; }
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int PriceRange { get; set; }
+    public string MenuLink { get; set; }
+    public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
   }
 }
